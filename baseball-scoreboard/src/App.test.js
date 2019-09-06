8af3d4e351874080155078b25e94a393bd51328a
Kenna <kennaalastair@gmail.com>
2019-09-05 17:50:55 -0700

    working on getting testig errors fixed

--- baseball-scoreboard/src/App.test.js
+++ baseball-scoreboard/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import 'jest-dom/extend-expect';
 import App from './App';
+
+import Dashboard from './components/Dashboard';
+import { ballCount, strikeCount, setBallCount, setStrikeCount } from './App';
+
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Dashboard renders without crashing', () => {
+  render(<Dashboard />);
+})
+
+test('contains Lions and Tigers scoreboards', () => {
+  // Arrange
+  const container = render(<App />);
+  console.log(container);
+  // Act - getting the node by text
+  container.getByText(/Baseball Scoreboard/i);
 });
